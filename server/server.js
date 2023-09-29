5e41974b9d83530e2c1373ac9616480e0e17b776
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-09-29 01:50:41 +0100

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", function (request, response) {
+  response.send("Dream big :)");
+});
+
+
+app.get("/sessions", function (request, response) {
+  response.status(200).json(quotes);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}. Ready to accept requests!`);
+});

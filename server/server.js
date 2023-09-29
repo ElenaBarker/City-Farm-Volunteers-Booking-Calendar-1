8a2e264ad5b2f500d494d4f0acf6cdf3ba73fe6d
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-09-29 02:59:47 +0100

    minor changes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", function (request, response) {
-  response.send("Dream big :)");
+  response.send("Dream big :)))");
 });
 
 

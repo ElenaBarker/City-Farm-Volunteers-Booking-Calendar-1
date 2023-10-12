61a7c5759455cd8f808b37ee3671ee365e3331d2
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-10-12 22:36:07 +0100

    added 2 buttons "volunteer" and "manager"

--- city-farm/src/HeaderComponent.js
+++ city-farm/src/HeaderComponent.js
@@ -1,10 +1,24 @@
 import logo from "./logo.png"
 
-const HeaderComponent = () => {
+const HeaderComponent = ({setPageToShow}) => {
     return (
       <header>
         <img className="App-logo" src={logo} alt="Logo" />
         WELCOME TO OUR CITY FARM
+        <button
+          onClick={() => {
+            setPageToShow("volunteer");
+          }}
+        >
+          Volunteer
+        </button>
+        <button
+          onClick={() => {
+            setPageToShow("manager");
+          }}
+        >
+          Manager
+        </button>
       </header>
     );
 };

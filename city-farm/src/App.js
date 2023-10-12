23b9efe0add4b5a5641cfa017606b6e749dcb21c
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-10-12 22:40:49 +0100

    UseState is used to initialise and update the "pageToShow" variable, added "if" statements

--- city-farm/src/App.js
+++ city-farm/src/App.js
@@ -1,21 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 import CalendarComponent from "./CalendarComponent";
 import FooterComponent from "./FooterComponent";
 import HeaderComponent from "./HeaderComponent";
 import VolunteerInstructionsComponent from "./VolunteerInstructionsComponent";
 
 function App() {
+  // allowed values "volunteer" or "manager"
+  const [pageToShow, setPageToShow] = useState("volunteer");
   return (
-    <div
-      className="App"
-      style={{
-        backgroundImage: `url(https://images.pexels.com/photos/36347/cow-pasture-animal-almabtrieb.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
-      
-      }}
-    >
-      <HeaderComponent />
-      <VolunteerInstructionsComponent />
-      <CalendarComponent />
+    <div className="App">
+      <HeaderComponent setPageToShow={setPageToShow} />
+      <VolunteerInstructionsComponent pageToShow={pageToShow} />
+      {pageToShow === "volunteer" ? <CalendarComponent /> : "Hello manager"}
       <FooterComponent />
     </div>
   );

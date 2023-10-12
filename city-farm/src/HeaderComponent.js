9bc1aa7bb89fe5bb1c658551a5cd4c74449d6156
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-10-12 23:04:51 +0100

    style buttons in header component

--- city-farm/src/HeaderComponent.js
+++ city-farm/src/HeaderComponent.js
@@ -1,11 +1,15 @@
-import logo from "./logo.png"
+import logo from "./logo.png";
 
-const HeaderComponent = ({setPageToShow}) => {
-    return (
-      <header>
+const HeaderComponent = ({ setPageToShow }) => {
+  return (
+    <header className="header-container">
+      <div className="header-content">
         <img className="App-logo" src={logo} alt="Logo" />
-        WELCOME TO OUR CITY FARM
+        <h1 className="farm-name">Welcome to our CITY FARM</h1>
+      </div>
+      <div className="button-container">
         <button
+          className="header-button"
           onClick={() => {
             setPageToShow("volunteer");
           }}
@@ -13,14 +17,16 @@ const HeaderComponent = ({setPageToShow}) => {
           Volunteer
         </button>
         <button
+          className="header-button"
           onClick={() => {
             setPageToShow("manager");
           }}
         >
           Manager
         </button>
-      </header>
-    );
+      </div>
+    </header>
+  );
 };
 
 export default HeaderComponent;

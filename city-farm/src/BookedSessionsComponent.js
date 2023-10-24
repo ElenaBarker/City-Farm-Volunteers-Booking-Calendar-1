4846d4b080ebcec24629733619be9ce099c679c5
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-10-24 21:58:47 +0100

    Update BookedSessionsComponent.js

--- city-farm/src/BookedSessionsComponent.js
+++ city-farm/src/BookedSessionsComponent.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-
 const BookedSessionsComponent = ({ bookedSessions }) => {
+  console.log(bookedSessions);
   return (
     <div className="booked-sessions">
       <ul>
         {bookedSessions.map((session, index) => (
           <li key={index} className="booked-sessions-item">
-            <strong>{session.title}</strong> - {session.startdate} -{" "}
-            {session.name}{" "}
+            <strong>{session.title}</strong> -{" "}
+            {new Date(session.startdate).toDateString()} - {session.name}{" "}
             <button className="cancel-booking-button">Cancel booking</button>
           </li>
         ))}

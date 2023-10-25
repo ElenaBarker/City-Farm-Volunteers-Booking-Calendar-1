e850399c09a12229119a2c3028d0441ef8442946
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-10-25 02:46:18 +0100

    Deleted logo, swapped app background with header background

--- city-farm/src/HeaderComponent.js
+++ city-farm/src/HeaderComponent.js
@@ -1,12 +1,10 @@
-import logo from "./logo.png";
 
 const HeaderComponent = ({ setPageToShow }) => {
   return (
     <header className="header-container">
       <div className="header-content">
-        <img className="App-logo" src={logo} alt="Logo" />
         <h1 className="farm-name">Welcome to our CITY FARM</h1>
-      </div>
+        </div>
       <div className="button-container">
         <button
           className="header-button"

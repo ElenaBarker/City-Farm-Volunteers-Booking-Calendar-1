aa061a5de8c61ba1159fa9f9f00bca2067e46c69
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-10-30 05:10:48 +0000

    Created new component Available Sessions for displaying it on manager view.

---
+++ city-farm/src/AvailableSessionsComponent.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+} from "@mui/material";
+
+const AvailableSessionsComponent = ({ availableSessions }) => {
+  return (
+    <div className="manager-view-container">
+      <h3 className="table-title">Available Sessions</h3>
+      <div className="table-content">
+        <TableContainer component={Paper} className="available-sessions">
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className="cell-names">Session Name</TableCell>
+                <TableCell className="cell-names">Start Date</TableCell>
+                <TableCell className="cell-names">Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {availableSessions.map((session) => (
+                <TableRow key={session.id}>
+                  <TableCell>{session.title}</TableCell>
+                  <TableCell>
+                    {new Date(session.start).toDateString()}
+                  </TableCell>
+                  <TableCell>{session.status}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+};
+
+export default AvailableSessionsComponent;
